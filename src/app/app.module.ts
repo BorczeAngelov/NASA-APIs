b83e88d4ca7ce418cb2b0abdbc2daa8a25b2e891
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-04-20 09:24:43 +0200

    g  c nasa-apod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NasaApodComponent } from './nasa-apod/nasa-apod.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NasaApodComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nasa-apod/nasa-apod.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nasa-apod',
+  templateUrl: './nasa-apod.component.html',
+  styleUrls: ['./nasa-apod.component.css']
+})
+export class NasaApodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
