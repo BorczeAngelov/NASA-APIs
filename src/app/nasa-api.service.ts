ff49a30a472cab303fb23c4eec48247bc65a7860
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-04-21 15:26:00 +0200

    update marsPhotoUrl

--- src/app/nasa-api.service.ts
+++ src/app/nasa-api.service.ts
@@ -1,20 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, pipe } from 'rxjs';
+import { Observable } from 'rxjs';
 import { DtoGetPhotos } from './mars-rovers-photos/DataModel/DtoGetPhotos';
-import { MarsPhoto } from './mars-rovers-photos/DataModel/MarsPhoto';
 import { ReturnObject } from './nasa-apod/DtoApod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NasaApiService {
-
   apodUrl: string = "https://api.nasa.gov/planetary/apod";
   // api_key: string = "DEMO_KEY"; //Note: you can replace value with your own api_key
   api_key: string = "GylTbFmuea3pDiZVRBJkzqCzLiC9VE2hYhHSegyc";
 
-  marsPhotosUrl: string = "https://mars-photos.herokuapp.com/api/v1"
+  // marsPhotosUrl: string = "https://mars-photos.herokuapp.com/api/v1" //no need for api_key
+  marsPhotosUrl: string = "https://api.nasa.gov/mars-photos/api/v1"
   roverName = "Perseverance";
 
   constructor(private http: HttpClient) { }
@@ -28,8 +27,9 @@ export class NasaApiService {
     return this.http.get<ReturnObject>(this.apodUrl + '?api_key=' + this.api_key + "&date=" + date);
   }
 
-  getMarsRoverPhotos(marsDay: string): Observable<DtoGetPhotos> {    
-    return this.http.get<DtoGetPhotos>(this.marsPhotosUrl + "/rovers/" + this.roverName + "/photos?sol=" + marsDay);
+  getMarsRoverPhotos(marsDay: string): Observable<DtoGetPhotos> {
+    return this.http.get<DtoGetPhotos>(this.marsPhotosUrl + "/rovers/" + this.roverName + "/photos?" +
+      'api_key=' + this.api_key + "&sol=" + marsDay);
   }
 }
 
